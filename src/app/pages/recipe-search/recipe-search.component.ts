f38031899ff366736f41b7119c521ce913ca28ee
Frederik Wissing <vestervang-rs@hotmail.com>
2024-12-18 08:39:57 +0100

    Added main and recipe page

---
+++ src/app/pages/recipe-search/recipe-search.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-search',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-search.component.html',
+  styleUrl: './recipe-search.component.scss'
+})
+export class RecipeSearchComponent {
+
+}
